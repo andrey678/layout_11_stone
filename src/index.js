a9ca923c6df233f23db5bccfd336b07935dc5de6
andrey678 <drew6496@yandex.ru>
2022-07-04 20:45:05 +0300

    1.Add global styles using styled-components 2.Add theme using styled-components

--- src/index.js
+++ src/index.js
@@ -1,13 +1,115 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+
+
 import App from './App';
 
+// Подключение Проводника тем и Глобальных стилей
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+
+const defaultTheme = {
+  colors: {
+    lightSand: "#C7AA8B",
+    darkBrown: "#323232",
+    darkGray: "#4D4D4D",
+  }
+
+}
+
+const Global = createGlobalStyle`
+* {
+  padding: 0;
+  margin: 0;
+  border: 0;
+}
+
+*,
+*:before,
+*:after {
+  -moz-box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+}
+
+:focus,
+:active {
+  outline: none;
+}
+a:focus,
+a:active {
+  outline: none;
+}
+
+nav,
+footer,
+header,
+aside {
+  display: block;
+}
+
+html,
+body {
+  height: 100%;
+  width: 100%;
+  font-size: 100%;
+  line-height: 1;
+  font-size: 14px;
+  -ms-text-size-adjust: 100%;
+  -moz-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+}
+
+input,
+button,
+textarea {
+  font-family: inherit;
+}
+
+input::-ms-clear {
+  display: none;
+}
+button {
+  cursor: pointer;
+}
+button::-moz-focus-inner {
+  padding: 0;
+  border: 0;
+}
+a,
+a:visited {
+  text-decoration: none;
+}
+a:hover {
+  text-decoration: none;
+}
+ul li {
+  list-style: none;
+}
+img {
+  vertical-align: top;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  font-size: inherit;
+  font-weight: 400;
+}
+
+`;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={defaultTheme}>
+      <Global />
+      <App />
+    </ThemeProvider>
+
   </React.StrictMode>
 );
 
